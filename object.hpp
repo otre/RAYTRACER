f47cba0c8ba04eea02e3e3b9ff0de675dd91a03d
loucas971 <loucas971@hotmail.com>
2013-03-02 18:58:38 +0100

    Update object.hpp

--- object.hpp
+++ object.hpp
@@ -8,7 +8,7 @@ class Object{
 
  public:
   // renvoie le test d'intersection i avec l'objet
-  virtual bool intersect(const vec3& ray, const vec3& eye, vec3& i)=0;
+  virtual bool isIntersected(const vec3& ray, const vec3& eye, vec3& i)=0;
 };
 
 #endif // OBJECT_HPP
